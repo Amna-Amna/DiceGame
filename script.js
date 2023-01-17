086c79ce633d0850ec087d2682c9933116ba53d3
Amna-Amna <mailtoamna4@gmail.com>
2023-01-17 17:21:00 +0500

    Used Ids somewhere instead of classes

--- script.js
+++ script.js
@@ -2,28 +2,20 @@
 
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const scoreEl = document.querySelectorAll('.score');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const diceEl = document.querySelector('.dice');
 
-diceEl.classList.add('hidden');
-
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
-};
 
 for (let i = 0; i < scoreEl.length; i++) {
   scoreEl[i].textContent = 0;
@@ -46,30 +38,52 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-btnHold.addEventListener('click', function () {
-  if (playing) {
-    score[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      score[activePlayer];
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-    if (score[activePlayer] >= 20) {
-      playing = false;
-      diceEl.classList.add('hidden');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--active');
-    } else {
-      switchPlayer();
-    }
-  }
-});
-// const score0El = document.querySelector('#score--0');
-// const score1El = document.querySelector('#score--1');
-// const diceEl = document.querySelector('.dice');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-// score0El.textContent = 0;
-// score1El.textContent = 0;
-// diceEl.classList.add('hidden');
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+// init();
+// btnHold.addEventListener('click', function () {
+//   if (playing) {
+//     scores[activePlayer] += currentScore;
+
+//     document.getElementById(`score--${activePlayer}`).textContent =
+//       scores[activePlayer];
+
+//     if (scores[activePlayer] >= 100) {
+//       playing = false;
+//       diceEl.classList.add('hidden');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.add('player--winner');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.add('player--active');
+//     } else {
+//       switchPlayer();
+//     }
+//   }
+// });
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// btnNew.addEventListener('click', init);
